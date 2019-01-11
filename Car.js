2a9913b8f89a8f8f891e1650947aad1d21f60d35
Computer <computer@Computers-MacBook-Pro.local>
2019-01-11 16:19:52 -0700

    finished

--- Car.js
+++ Car.js
@@ -1,5 +1,16 @@
-class Car{
-    
+class Car {
+    constructor(color, convertible) {
+        this.color = color;
+        this.convertible = convertible;
+        this.speed = 0;
+    }
+    accelerate(a) {
+        this.speed += a
+    }
+    decelerate(d) {
+        this.speed -= d
+    }
+
 }
 
 module.exports = Car;
